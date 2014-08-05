90b33e3873e1faed040df666396ff9f438f89348
paldom <paldomonkos@gmail.com>
2014-08-05 16:15:28 +0200

    java fix

--- src/android/NumericKeyboard.java
+++ src/android/NumericKeyboard.java
@@ -3,32 +3,41 @@ package hu.dpal.phonegap.plugins;
 import org.apache.cordova.CallbackContext;
 import org.apache.cordova.CordovaPlugin;
 import org.json.JSONArray;
+import org.json.JSONException;
 
+import android.app.Activity;
 import android.view.ViewGroup;
 import android.widget.RelativeLayout;
 import android.widget.RelativeLayout.LayoutParams;
 import android.widget.TableLayout;
 
-public class UniqueDeviceID extends CordovaPlugin {
+public class NumericKeyboard extends CordovaPlugin {
 
     @Override
-    public boolean execute(String action, JSONArray args, CallbackContext callbackContext) {
+    public boolean execute(String action, JSONArray args, CallbackContext callbackContext) throws JSONException {
 
-        ViewGroup cordovaViewGroup;
-        RelativeLayout wrapperView;
-        TableLayout numpadView;
+        ViewGroup cordovaViewGroup = null;
+        RelativeLayout wrapperView = null;
+        TableLayout numpadView = null;
+        Activity cdvActivity;
         
         if (action.equals("show")) {
             
             String id = args.getString(0);
             
+            cdvActivity = cordova.getActivity();
+            
             wrapperView = new RelativeLayout(cordova.getActivity());
             cordovaViewGroup = (ViewGroup) webView.getParent();
             cordovaViewGroup.removeView(webView);
             wrapperView.addView(webView);
-            cordova.getActivity().setContentView(wrapperView);
+            cdvActivity.setContentView(wrapperView);
             
-            numpadView = (TableLayout)findViewById(R.id.tblNumpad);
+            numpadView = (TableLayout) cdvActivity.findViewById(
+            		cdvActivity.getApplication().getResources().getIdentifier(
+            				"numpad",
+            				"layout",
+            				cdvActivity.getApplication().getPackageName()));
             
             RelativeLayout.LayoutParams lparams = new RelativeLayout.LayoutParams(
                                 LayoutParams.MATCH_PARENT, LayoutParams.WRAP_CONTENT);
@@ -37,17 +46,21 @@ public class UniqueDeviceID extends CordovaPlugin {
             lparams.addRule(RelativeLayout.CENTER_HORIZONTAL,
                                 RelativeLayout.TRUE);
             
-            numpadView.setLayoutParams(params);
+            numpadView.setLayoutParams(lparams);
             wrapperView.addView(numpadView);
             
             return true;
         } else if (action.equals("hide")) {
             
-            wrapperView.removeView(webView);
-            wrapperView.removeView(numpadView);
-            cordovaViewGroup.addView(webView);
-            cordova.getActivity().setContentView(cordovaViewGroup);
-            wrapperView = null;
+        	if (wrapperView != null) {
+	            wrapperView.removeView(webView);
+	            wrapperView.removeView(numpadView);
+	            cordovaViewGroup.addView(webView);
+	            cdvActivity.setContentView(cordovaViewGroup);
+	            wrapperView = null;
+                cdvActivity = null;
+        	}
+            
             
             return true;
         }
